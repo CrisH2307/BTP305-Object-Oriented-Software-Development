6e88875a392920134bf73f8284c81d8a6a97411a
Cris Huynh <119613966+CrisH2307@users.noreply.github.com>
2023-09-13 13:51:09 -0400

    push

--- BTP-Workshops/WS01/part 2/RideRequest.h
+++ BTP-Workshops/WS01/part 2/RideRequest.h
@@ -21,7 +21,7 @@ namespace sdds
     class RideRequest
     {
     private: 
-        std::string m_customer;
+        char m_customer[11];
         std::string m_detail;
         double m_price;
         bool m_discount;
